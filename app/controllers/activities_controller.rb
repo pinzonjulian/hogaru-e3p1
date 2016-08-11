7f5a4bb9f5af01225d6f27584b7000277364ee15
pinzonjulian <julianpinzoneslava@gmail.com>
2016-08-11 17:03:22 -0500

    Add update edit create and new controller actions. Add forms for new and edit. Needs refactoring

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,14 +1,30 @@
 class ActivitiesController < ApplicationController
   def new
+    @activity = Activity.new
   end
 
   def create
+    @activity = Activity.new(activity_params)
+    if @activity.save
+      flash[:success] = "Activity registered!"
+      redirect_to @activity
+    else
+      render
+    end
   end
 
   def update
+    @activity = Activity.find(params[:id])
+    if @activity.update_attributes(activity_params)
+      flash[:success] = "Activity updated!"
+      redirect_to @activity
+    else
+      render 'edit'
+    end
   end
 
   def edit
+    @activity = Activity.find(params[:id])
   end
 
   def destroy
@@ -19,4 +35,12 @@ class ActivitiesController < ApplicationController
 
   def show
   end
+
+  private
+
+    def activity_params
+      params.require(:activity).permit(:name, :cal_intake,
+                                       :cal_burnt, :date )
+    end
+
 end
