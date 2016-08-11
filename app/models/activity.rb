d2b3f6c4f8fa1b0402f4a8f2290f5a7be9d97228
pinzonjulian <julianpinzoneslava@gmail.com>
2016-08-11 17:22:10 -0500

    Add validations to Activity Model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,11 @@
 class Activity < ApplicationRecord
   # belongs_to :user
+  validates :name, presence: true, length: { minimum: 3, maximum: 140 }
+  validates :date, presence: true
+  validates :cal_burnt, presence: true,
+                        numericality: { greater_than_or_equal_to: 0,
+                                        less_than_or_equal_to: 5000 }
+  validates :cal_intake, presence: true,
+                         numericality: { greater_than_or_equal_to: 0,
+                                         less_than_or_equal_to: 5000 }
 end
