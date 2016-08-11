b358c4b708fc5aa48fad54fe9dee3dc1155e5d94
pinzonjulian <julianpinzoneslava@gmail.com>
2016-08-11 16:37:52 -0500

    Fix controller name. Add resources in routes for activities

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,7 @@
 Rails.application.routes.draw do
-  get 'activity_controller/new'
-
-  get 'activity_controller/create'
-
-  get 'activity_controller/update'
-
-  get 'activity_controller/edit'
-
-  get 'activity_controller/destroy'
-
-  get 'activity_controller/index'
-
-  get 'activity_controller/show'
-
-  devise_for :users
   root 'static_pages#home'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :activities
 
 end
--- test/controllers/activities_controller_test.rb
+++ test/controllers/activities_controller_test.rb
@@ -1,38 +1,38 @@
 require 'test_helper'
 
-class ActivityControllerControllerTest < ActionDispatch::IntegrationTest
+class ActivitiesControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get activity_controller_new_url
+    get activities_new_url
     assert_response :success
   end
 
   test "should get create" do
-    get activity_controller_create_url
+    get activities_create_url
     assert_response :success
   end
 
   test "should get update" do
-    get activity_controller_update_url
+    get activities_update_url
     assert_response :success
   end
 
   test "should get edit" do
-    get activity_controller_edit_url
+    get activities_edit_url
     assert_response :success
   end
 
   test "should get destroy" do
-    get activity_controller_destroy_url
+    get activities_destroy_url
     assert_response :success
   end
 
   test "should get index" do
-    get activity_controller_index_url
+    get activities_index_url
     assert_response :success
   end
 
   test "should get show" do
-    get activity_controller_show_url
+    get activities_show_url
     assert_response :success
   end
 
