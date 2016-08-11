46ef99ec6f688162e4dfed133d93195a4eabc35a
pinzonjulian <julianpinzoneslava@gmail.com>
2016-08-11 16:30:01 -0500

    Add ActivityController

---
+++ app/helpers/activity_controller_helper.rb
@@ -0,0 +1,2 @@
+module ActivityControllerHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'activity_controller/new'
+
+  get 'activity_controller/create'
+
+  get 'activity_controller/update'
+
+  get 'activity_controller/edit'
+
+  get 'activity_controller/destroy'
+
+  get 'activity_controller/index'
+
+  get 'activity_controller/show'
+
   devise_for :users
   root 'static_pages#home'
 
